a440ccbdd497b4342c40dac3cb17809e173dc9f4
Rodney <rodsan.724@outlook.com>
2018-06-10 23:12:21 -0400

    sequelize tutorial initial check in

---
+++ app.js
@@ -0,0 +1,51 @@
+require('dotenv/config');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+
+});
+
+// Or you can simply use a connection uri
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+    const User = sequelize.define('user', {
+        firstName: {
+          type: Sequelize.STRING
+        },
+        lastName: {
+          type: Sequelize.STRING
+        }
+      });
+      
+      // force: true will drop the table if it already exists
+    //   User.sync({force: true}).then(() => {
+    //     // Table created
+    //     return User.create({
+    //       firstName: 'John',
+    //       lastName: 'Hancock'
+    //     });
+    //   });
+
+
+      User.findAll().then(users => {
+        console.log(users)
+      })
